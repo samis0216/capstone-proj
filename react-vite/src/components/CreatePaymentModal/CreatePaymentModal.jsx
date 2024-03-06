5518fcebfbb450239dc3393fe513df6f16f57a1c
Sami <samisamman26@yahoo.com>
2024-03-06 13:15:29 -0800

    fix #6

--- react-vite/src/components/CreatePaymentModal/CreatePaymentModal.jsx
+++ react-vite/src/components/CreatePaymentModal/CreatePaymentModal.jsx
@@ -14,8 +14,8 @@ export default function CreatePaymentModal() {
             <div className="paymentModalMiddle">
                 <p>Choose a payment method</p>
                 <button className='paymentOption' id='record' onClick={()=> {closeModal(); navigate('/payments/new')}}>Record cash payment</button>
-                <button className='paymentOption' id='paypal' onClick={()=> alert('Feature coming soon...')}><img src="https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg" alt="" style={{width: 100}}/></button>
-                <button className='paymentOption' id='venmo' onClick={() => alert('Feature coming soon...')}><img src="https://download.logo.wine/logo/Venmo/Venmo-Logo.wine.png" alt="" style={{width: 100}}/></button>
+                {/* <button className='paymentOption' id='paypal' onClick={()=> alert('Feature coming soon...')}><img src="https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg" alt="" style={{width: 100}}/></button>
+                <button className='paymentOption' id='venmo' onClick={() => alert('Feature coming soon...')}><img src="https://download.logo.wine/logo/Venmo/Venmo-Logo.wine.png" alt="" style={{width: 100}}/></button> */}
                 <p style={{color: 'gray'}}>Splitti charges no extra fees.</p>
             </div>
 
