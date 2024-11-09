6d44a1bb75c8576e97ee49e4b1a7208b45b30f81
Sami <samisamman26@yahoo.com>
2024-11-09 12:50:03 -0400

    added imports and backgroudn color spec

---
+++ app/api/downloader.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+import tkinter as tk
+from tkinter import messagebox
+import threading
+import re  # Import the regular expressions module
+
+# Custom color scheme
+BG_COLOR = "#1e1e1e"  # Dark background color
+TEXT_COLOR = "#c5c5c5"  # Light text color for readability
+ENTRY_COLOR = "#2d2d2d"  # Slightly lighter color for entry fields
