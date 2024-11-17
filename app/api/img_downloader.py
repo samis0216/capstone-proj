2c8db266f69e3474e09991ebc4a809ebdefc4424
Sami <samisamman26@yahoo.com>
2024-11-16 20:24:20 -0400

    cleaned up

--- app/api/img_downloader.py
+++ app/api/img_downloader.py
@@ -1,18 +1,17 @@
 import requests
 
 def download_picture():
-    # Prompt the user for the picture URL
+
     url = input("Enter the picture URL: ").strip()
 
-    # Prompt the user for the name to save the file as
+
     file_name = input("Enter the filename to save as (including extension, e.g., image.jpg): ").strip()
 
     try:
-        # Send a GET request to the URL
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
 
-        # Write the content to a file
+        response = requests.get(url)
+        response.raise_for_status()
+
         with open(file_name, 'wb') as file:
             file.write(response.content)
 
